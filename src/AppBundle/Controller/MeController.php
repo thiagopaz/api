cf7776d7b27288c6a45060f0e5524bc1d37a8498
Thiago Paz <luiz.paz86@gmail.com>
2017-09-01 17:17:48 -0300

    ok

---
+++ src/AppBundle/Controller/MeController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use JMS\Serializer\SerializerBundle;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
+class MeController extends Controller
+{  
+     /**
+    * RESTFul action which returns all students   
+    * @Route("api/me.json/{id}")
+    * @Method("GET")
+    * @ApiDoc(
+    *  resource = "true",
+    *  description = "My data " ,
+    *  output = "\AppBundle\Entity\User",
+    *  statusCodes = {
+    *      200 = "Returned when successful" ,
+    *      400 = "Returned when the page is not found"   
+    *  }
+    * )
+    *      
+    * @return Response{'status and data'}
+    */
+    public function getMe(Request $request){
+        $user = $this->getUser();
+        if($request->get('id') != $user->getId()){
+            throw new AccessDeniedException('This user does not have access to this section.');      
+        }
+        $repository = $this->getDoctrine()->getRepository("AppBundle:Students"); 
+        try{
+            $me    = $repository->findBy(array('id'   => $request->get('id') ));
+        }catch(\Exception $exception){
+            $me = NULL;
+        }
+        $result = array('status' => true , 'data' => $me);
+        $serializer = $this->container->get('jms_serializer');       
+        $data   = $serializer->serialize($result, 'json');
+        return new Response($data);  
+    }
+
+         /**
+    * RESTFul action which returns all students   
+    * @Route("api/me.json/{id}")
+    * @Method("POST")
+    * @ApiDoc(
+    *  resource = "true",
+    *  description = "My data " ,
+    *  output = "\AppBundle\Entity\User",
+    *  statusCodes = {
+    *      200 = "Returned when successful" ,
+    *      400 = "Returned when the page is not found"  
+    *  }
+    * )
+    *      
+    * @return Response{'status and data'}
+    */
+    public function updateMe(Request $request){
+        $user = $this->getUser();
+        if($request->get('id') != $user->getId()){
+            throw new AccessDeniedException('This user does not have access to this section.');      
+        }
+        $repository = $this->getDoctrine()->getRepository("AppBundle:Students"); 
+        try{
+            $me    = $repository->findBy(array('id'   => $request->get('id') ));
+        }catch(\Exception $exception){
+            $me = NULL;
+        }
+        $result = array('status' => true , 'data' => $me);
+        $serializer = $this->container->get('jms_serializer');       
+        $data   = $serializer->serialize($result, 'json');
+        return new Response($data);  
+    }
+}
--- src/AppBundle/Entity/Teachers.php
+++ src/AppBundle/Entity/Teachers.php
@@ -10,7 +10,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="teacher")
  * @ORM\Entity(repositoryClass="AppBundle\Repository\TeacherRepository")
  */
-class Teacher
+class Teachers
 {
     /**
      * @var int
